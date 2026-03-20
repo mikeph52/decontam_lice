ca298727739a768cb52e9afc4e0361376809394c
mikeph52 <mikeph526@outlook.com>
2026-03-20 10:42:56 +0200

    Sorting and converting BAM to FASTQ added

--- scripts/bamtofastq.sh
+++ scripts/bamtofastq.sh
@@ -3,12 +3,17 @@
 #SBATCH --partition=batch
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
-#SBATCH --mem=40000              
+#SBATCH --mem=45GB              
 #SBATCH --job-name="bamtofastq"
 #SBATCH --output=/home1/mikeph/project_data/decontam_lice/logs/samtools/bamtofastq.output
 #SBATCH --mail-user=mikeph526@outlook.com 
 #SBATCH --mail-type=ALL
 
 DATA=/home1/mikeph/data/hifi_reads/Cal_mix.hifi_reads.bam
+DIR=/home1/mikeph/data/hifi_reads/
 
-samtools fastq -@ 20 "$DATA" | bgzip -@ 20 > Cal_mix.fastq.gz
+echo "SORTING STARTED"
+samtools sort -n -@ 20 -m 2G -o "$DIR"/Cal_mix_sorted.bam "$DATA"
+echo "SORTING ENDED, CONVERSION STARTED"
+samtools fastq -@ 10 "$DIR"/Cal_mix_sorted.bam | bgzip -@ 10 > "$DIR"/Cal_mix.fastq.gz
+echo "CONVERTION ENDED"
