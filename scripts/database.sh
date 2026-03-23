fb8cc60864ee7569e77f70e9cd34a50f082cd1e7
mikeph52 <mikeph526@outlook.com>
2026-03-23 10:25:29 +0200

    Figuring out a way to download the databases.

--- scripts/database.sh
+++ scripts/database.sh
@@ -16,12 +16,13 @@ conda activate ncbi
 
 echo "$(date):Downloading databases for NCBI to /home1/mikeph/data/kraken2_db."
 
+#Downloaded databases are commented out.
 datasets download genome taxon 6656  --reference --filename "$DB"/arthropoda.zip      
-datasets download genome taxon 1224  --reference --filename "$DB"/proteobacteria.zip  
+#datasets download genome taxon 1224  --reference --filename "$DB"/proteobacteria.zip  
 datasets download genome taxon 5878  --reference --filename "$DB"/ciliophora.zip      
 datasets download genome taxon 4890  --reference --filename "$DB"/ascomycota.zip      
 datasets download genome taxon 33090 --reference --filename "$DB"/streptophyta.zip    
-datasets download genome taxon 9606  --reference --filename "$DB"/human.zip           
+#datasets download genome taxon 9606  --reference --filename "$DB"/human.zip           
 datasets download genome taxon 6073  --reference --filename "$DB"/cnidaria.zip       
 
 echo "$(date):Datasets saved succesfully in /home1/mikeph/data/kraken2_db."
