850ba1462bdabb26a70a58bd3142ebdfbc53030d
mikeph52 <mikeph526@outlook.com>
2026-03-24 13:56:40 +0200

    Fix workflow.sh, delete unused scripts

--- workflow.sh
+++ workflow.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-#SBATCH --partition=batch
+#SBATCH --partition=fat
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
-#SBATCH --mem=40000              
+#SBATCH --mem=200GB              
 #SBATCH --job-name="kraken2"
-#SBATCH --output=logs/shell/kraken2.output
+#SBATCH --output=logs/kraken2/kraken2.output
 #SBATCH --mail-user=mikeph526@outlook.com 
 #SBATCH --mail-type=ALL
 
-snakemake -j 20
+DB=/home1/mikeph/data/kraken2_db/library/added
+FASTQ=/home1/mikeph/data/hifi_reads/Cal_mix.fastq.gz
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate ncbi
+
+set -euo pipefail
+
+kraken2 \
+  --db "$DB" \
+  --threads 20 \
+  --output results/kraken2/dec.kraken2.output \
+  --report results/kraken2/dec.kraken2.report \
+  --gzip-compressed "$FASTQ"
