6286d0381095dd07cc527ba83f93a1239ebdd1b8
mikeph52 <mikeph526@outlook.com>
2026-03-26 14:37:14 +0200

    Modified and run workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -4,22 +4,23 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
 #SBATCH --mem=200GB              
-#SBATCH --job-name="kraken2"
-#SBATCH --output=logs/kraken2/kraken2.output
+#SBATCH --job-name="kraken2_2"
+#SBATCH --output=logs/kraken2/kraken2_2.output
 #SBATCH --mail-user=mikeph526@outlook.com 
 #SBATCH --mail-type=ALL
 
-DB=/home1/mikeph/data/kraken2_db/library/added
+DB=/home1/mikeph/data/kraken2_db
 FASTQ=/home1/mikeph/data/hifi_reads/Cal_mix.fastq.gz
+
+set -euo pipefail
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ncbi
 
-set -euo pipefail
-
 kraken2 \
   --db "$DB" \
   --threads 20 \
-  --output results/kraken2/dec.kraken2.output \
-  --report results/kraken2/dec.kraken2.report \
+  --confidence 0.1\
+  --output results/kraken2/dec.kraken2_2.output \
+  --report results/kraken2/dec.kraken2_2.report \
   --gzip-compressed "$FASTQ"
