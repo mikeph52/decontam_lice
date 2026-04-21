44d6ff53f0835100120578984e7affd65b356f1b
mikeph52 <mikeph526@outlook.com>
2026-04-21 12:29:50 +0300

    Database needs approx. 5TB of storage

--- scripts/estimate_db_size.sh
+++ scripts/estimate_db_size.sh
@@ -17,23 +17,24 @@ export LANG=en_US.UTF-8
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ncbi # for ncbi toolset and kraken2
 
-echo "Searching estimated db size. . . "
+#echo "Searching estimated db size. . . "
 
-datasets download genome taxon 6656 --dehydrated --preview --no-progressbar
-datasets download genome taxon 1224 --dehydrated --preview --no-progressbar
-datasets download genome taxon 5878 --dehydrated --preview --no-progressbar
-datasets download genome taxon 4890 --dehydrated --preview --no-progressbar
-datasets download genome taxon 33090 --dehydrated --preview --no-progressbar
-datasets download genome taxon 9606 --dehydrated --preview --no-progressbar
-datasets download genome taxon 6073 --dehydrated --preview --no-progressbar
-datasets download genome taxon 6830 --dehydrated --preview --no-progressbar
-datasets download genome taxon 7711 --dehydrated --preview --no-progressbar
-datasets download genome taxon 4751 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 6656 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 1224 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 5878 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 4890 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 33090 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 9606 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 6073 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 6830 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 7711 --dehydrated --preview --no-progressbar
+#datasets download genome taxon 4751 --dehydrated --preview --no-progressbar
 
 echo "Searching estimated db size for reference genomes only. . . "
 
+echo "Searching for Arthropoda. . . "
 datasets download genome taxon 6656 --dehydrated --preview --reference --no-progressbar
-datasets download genome taxon 1224 --dehydrated --preview --reference --no-progressbar
+#datasets download genome taxon 1224 --dehydrated --preview --reference --no-progressbar
 datasets download genome taxon 5878 --dehydrated --preview --reference --no-progressbar
 datasets download genome taxon 4890 --dehydrated --preview --reference --no-progressbar
 datasets download genome taxon 33090 --dehydrated --preview --reference --no-progressbar
