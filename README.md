5149a3856f73881c56d863259a2270218d1a43af
mikeph52 <mikeph526@outlook.com>
2026-04-22 13:55:20 +0300

    Init workflow_pol.sh

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Workflow for decontamination of genome assembly of _Caligus minimus_
+
+## Introduction
+
+This workflow is intented for genome decontamination of _Caligus minimus_, a species of the _Caligidae_ family  
+
+
+## Depedencies
+- Biogenie
+- Kraken2
+- seqtk
--- workflow_pol.sh
+++ workflow_pol.sh
@@ -13,8 +13,8 @@
 
 DB=/home1/mikeph/data/kraken2_db
 FASTA=/home1/mikeph/project_data/caligus_assembly/results/flye/40-polishing/polished_1.fasta
-FASTQ=/home1/mikeph/data/hifi_reads/Cal_mix.fastq.gz
 POL_RESULTS=/home1/mikeph/project_data/decontam_lice/results/kraken2/polished1
+RESULTS=/home1/mikeph/project_data/decontam_lice/results
 
 set -euo pipefail
 
@@ -24,6 +24,9 @@ export LANG=en_US.UTF-8
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ncbi
 
+echo "Decontamination workflow of Caligus minimus."
+echo "$(date): Running kraken2. . ."
+
 kraken2 \
   --db "$DB" \
   --threads 20 \
@@ -31,3 +34,35 @@ kraken2 \
   --output "$POL_RESULTS"/dec.kraken2_4.output \
   --report "$POL_RESULTS"/dec.kraken2_4.report \
   "$FASTA"
+
+echo "$(date): Kraken2 completed."
+echo "$(date): Extraction of unclassified contig IDs started."
+
+awk '$1 == "U" {print $2}' "$POL_RESULTS"/dec.kraken2_4.output > "$RESULTS"/awk/unclassified_contigs.txt
+
+echo "$(date): Extraction of unclassified contig IDs finished. Saved at ${$RESULTS}/awk/unclassified_contigs.txt "
+echo "$(date): Removal of contaminants with seqtk started."
+
+seqtk subseq "$FASTA" "$RESULTS"/awk/unclassified_contigs.txt > "$RESULTS"/seqtk/polished_1_decontam.fasta
+
+echo "$(date): Removal of contaminants with seqtk completed."
+echo "$(date): Decontamination verification started."
+echo "$(date): Running biogenie. . ."
+
+biogenie -sh "$FASTA"
+echo "------------------------------------------------------------------------------------------------------"
+biogenie -sh "$RESULTS"/seqtk/polished_1_decontam.fasta
+
+echo "$(date): Biogenie completed."
+echo "$(date): Running kraken2. . ."
+
+kraken2 \
+  --db "$DB" \
+  --threads 20 \
+  --confidence 0.1\
+  --output "$POL_RESULTS"/decontaminated_kraken2.output \
+  --report "$POL_RESULTS"/decontaminated_kraken2.report \
+  "$RESULTS"/seqtk/polished_1_decontam.fasta
+
+  echo "$(date): Decontamination verification finished."
+  echo "$(date): Job finished."
