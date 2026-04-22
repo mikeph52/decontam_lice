56759afc19d7ae125e258cbb685b1a077cb303cf
mikeph52 <mikeph526@outlook.com>
2026-04-22 11:29:37 +0300

    Add workflow_pol.sh to test on polished_1 data

---
+++ workflow_pol.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --partition=fat
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=200GB              
+#SBATCH --job-name="kraken2"
+#SBATCH --output=logs/kraken2/kraken2_pol.output
+#SBATCH --mail-user=mikeph526@outlook.com 
+#SBATCH --mail-type=ALL
+
+DB=/home1/mikeph/data/kraken2_db
+FASTA=/home1/mikeph/project_data/caligus_assembly/results/flye/40-polishing/polished_1.fasta
+FASTQ=/home1/mikeph/data/hifi_reads/Cal_mix.fastq.gz
+POL_RESULTS=/home1/mikeph/project_data/decontam_lice/results/kraken2/polished1
+
+set -euo pipefail
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate ncbi
+
+kraken2 \
+  --db "$DB" \
+  --threads 20 \
+  --confidence 0.1\
+  --output "$POL_RESULTS"/dec.kraken2_4.output \
+  --report "$POL_RESULTS"/dec.kraken2_4.report \
+  "$FASTA"
