3fb199cec9bfe9354b144b3ecda1c6739d86c18e
mikeph52 <mikeph526@outlook.com>
2026-05-14 11:18:42 +0300

    Fix file bug on workflow.sh and update depedencies on environment.yaml

--- workflow.sh
+++ workflow.sh
@@ -21,19 +21,6 @@ export LANG=en_US.UTF-8
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ncbi
 
-#echo "Running Kraken2 on raw reads..."
-
-#kraken2 \
-#  --db "$DB" \
-#  --threads 30 \
-#  --confidence 0.1\
-#  --output results/kraken2/raw/kraken2_raw.output \
-#  --report results/kraken2/raw/kraken2_raw.report \
-#  --gzip-compressed "$FASTQ"
-
-#echo "Kraken2 run on raw reads finished."
-#echo "Report saved at 'results/kraken2/raw/kraken2_raw.report'."
-#echo " "
 echo "Running Kraken2 on hifiasm assembly..."
 
 kraken2 \
@@ -41,7 +28,7 @@ kraken2 \
   --threads 30 \
   --confidence 0.1\
   --output results/kraken2/hifiasm_assembly/kraken2_hifiasm.output \
-  --report results/kraken2/hifiasm_assembly/kraken2_hifiasmreport \
+  --report results/kraken2/hifiasm_assembly/kraken2_hifiasm.report \
   "$FASTA"
 
 echo "Kraken2 run on hifiasm assembly finished."
